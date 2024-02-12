39d0aa59f31c706504318f7237352f091c5807ce
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2024-02-12 18:23:30 -0500

    Update tab content using JavaScript
    Created index.js to change the tab's content based on which navigation
    link is clicked from the Homepage.

---
+++ Assets/JS/index.js
@@ -0,0 +1,36 @@
+/* Update tab content for links within index.html */
+
+const head = document.querySelector("head");
+const tab = document.querySelector("title");
+const homeLink = document.querySelector(".home-link");
+const aboutLink = document.querySelector(".about-link");
+const hoursLink = document.querySelector(".hours-link");
+const contactLink = document.querySelector(".contact-link");
+const locationLink = document.querySelector(".location-link");
+const faqLink = document.querySelector(".faq-link");
+
+homeLink.addEventListener('click', () => {
+    tab.textContent = "Crystal Thai Restaurant";
+});
+
+aboutLink.addEventListener('click', () => {
+    tab.textContent = "Crystal Thai - About Us";
+});
+
+hoursLink.addEventListener('click', () => {
+    tab.textContent = "Crystal Thai Hours";
+});
+
+contactLink.addEventListener('click', () => {
+    tab.textContent = "Crystal Thai - Contact";
+});
+
+locationLink.addEventListener('click', () => {
+    tab.textContent = "Crystal Thai - Location";
+});
+
+faqLink.addEventListener('click', () => {
+    tab.textContent = "Crystal Thai - FAQs";
+});
+
+head.appendChild(tab);
