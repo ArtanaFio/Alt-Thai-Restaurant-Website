e9e675000f979000c985b1eeea7cbb60c399cdcd
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2024-02-22 00:53:46 -0500

    Create modal for alerts and update menu content
    Updated the lunch and dinner appetizer web pages with new information
    regarding a menu item. Created a modal or popup box advertising the menu
    change. Styled the modal with CSS, programmed the box to appear on
    on screen one second after homepage loads so that users realize it is a
    popup box. Applied an event listener to the "close" div element to allow
    users to close the popup box to better navigate the page after reading
    the popup's content. Updated the business hours section to state a
    special close-for-business date.

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -40,3 +40,20 @@ head.appendChild(tab);
 const copyrightYear = document.querySelector(".year");
 
 copyrightYear.textContent = new Date().getFullYear();
+
+/* Remove modal from screen */
+
+const popup = document.getElementById("alert-notice");
+const closeButton = document.querySelector(".close");
+
+closeButton.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
+/* Slide modal onto the screen */
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(function() {
+        popup.style.display = "block";
+    }, 1000);
+});
