1fbc841b159cfe46c6edb901676e9bb249c8f64b
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2024-02-25 16:34:57 -0500

    Add modal to menu web page and update index style
    Created a new popup window to bring attention to a change in schedule on
    the menu web page in case users ignore the business hours section on the
    homepage. Changed the font-size for business days in the business hours
    section to keep hours on one line on smaller screens. Updated the home-
    page modal style to ensure information is more visible.

--- Assets/JS/menu.js
+++ Assets/JS/menu.js
@@ -3,3 +3,20 @@
 const copyrightYear = document.querySelector(".year");
 
 copyrightYear.textContent = new Date().getFullYear();
+
+/* Remove modal from screen */
+
+const popup = document.getElementById("alert-hours");
+const closeButton = document.querySelector(".close");
+
+closeButton.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
+/* Slide modal onto the screen */
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(function() {
+        popup.style.display = "block";
+    }, 1000);
+});
