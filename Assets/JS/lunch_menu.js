80cbc73d9e77bb39a167c57bbc621c77d8a331d5
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2024-03-11 09:06:47 -0400

    Remove menu modal, update lunch menu modal style
    Removed the menu webpage's modal because it is no longer needed.
    Restyled the lunch menu modal to put more focus on the modal's
    information and make the close button more visible to users. Created a
    separate JavaScript file specifically for the lunch menu's modal.

--- Assets/JS/lunch_menu.js
+++ Assets/JS/lunch_menu.js
@@ -1,23 +1,17 @@
-/* Automatically update copyright year */
-
-const copyrightYear = document.querySelector(".year");
-
-copyrightYear.textContent = new Date().getFullYear();
-
 /* Remove modal from screen */
-/*
-const popup = document.querySelector(".modal");
+
+const popup = document.getElementById("lunch-specials");
 const closeButton = document.querySelector(".close");
 
 closeButton.addEventListener('click', () => {
     popup.style.display = 'none';
 });
-*/
+
 /* Allow modal to appear on screen after a delay */
-/*
+
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(function() {
         popup.style.display = "block";
     }, 1000);
 });
-*/
+
